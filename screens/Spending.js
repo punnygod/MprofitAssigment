d9c0c944bc69d678e7dfceaef65344ce2b5f0444
Siddhesh Chavan <siddhesh@logiqids.com>
2023-03-27 01:34:01 +0530

    chart highlight and tooltip implemented design to be improved

--- screens/Spending.js
+++ screens/Spending.js
@@ -10,6 +10,14 @@ import {
 } from 'react-native';
 import {LineChart} from 'react-native-chart-kit';
 import * as Progress from 'react-native-progress';
+import {
+  Rect,
+  Text as TextSVG,
+  Svg,
+  Defs,
+  Stop,
+  LinearGradient,
+} from 'react-native-svg';
 
 const back = require('../assets/back.png');
 const threedot = require('../assets/threedot.png');
@@ -73,6 +81,12 @@ const Spending = ({navigation}) => {
       color: '#42887C24',
     },
   ]);
+  const [tooltipPos, setTooltipPos] = useState({
+    x: 0,
+    y: 0,
+    visible: false,
+    value: 0,
+  });
 
   // Update state variables based on user input or API calls
   useEffect(() => {
@@ -91,16 +105,18 @@ const Spending = ({navigation}) => {
       maximumFractionDigits: 1,
     }).format(data);
   };
-  const getlabels=()=>{
-    return data.map(a => new Date(a.Date)).map(a=>a.toLocaleString('en-US', { month: 'short' }))
-  }
+  const getlabels = () => {
+    return data
+      .map(a => new Date(a.Date))
+      .map(a => a.toLocaleString('en-US', {month: 'short'}));
+  };
   const datset = {
     labels: getlabels(),
     datasets: [
       {
         data: data.map(a => a.Spending),
-        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, 
-        strokeWidth: 2.5, 
+        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        strokeWidth: 2.5,
       },
     ],
   };
@@ -111,13 +127,67 @@ const Spending = ({navigation}) => {
     backgroundGradientTo: '#fff',
     backgroundGradientToOpacity: 1,
     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-    strokeWidth: 2.5, 
+    strokeWidth: 2.5,
     barPercentage: 0.5,
-    useShadowColorFromDataset: false, 
+    useShadowColorFromDataset: false,
     fillShadowGradientTo: '#fff',
     fillShadowGradientFrom: '#fff',
     labelColor: (opacity = 1) => `#818181`,
   };
+
+  const decorator = () => {
+    return tooltipPos.visible ? (
+      <View>
+        <Svg height="256" width="40">
+          <Defs>
+            <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
+              <Stop offset="0" stopColor="#FFC72708" stopOpacity="0.03" />
+              <Stop offset="1" stopColor="#FFC72730" stopOpacity="0.19" />
+            </LinearGradient>
+          </Defs>
+          <Rect
+            x={tooltipPos.x - 20}
+            y={0}
+            width="40"
+            height="256"
+            fill="url(#grad)"
+          />
+        </Svg>
+        <Svg>
+          <Rect
+            x={tooltipPos.x - 15}
+            y={tooltipPos.y + 10}
+            rx="6"
+            width="50"
+            height="30"
+            fill="#718096"
+          />
+          <TextSVG
+            x={tooltipPos.x + 10}
+            y={tooltipPos.y + 30}
+            fill="white"
+            fontSize="12"
+            fontWeight="bold"
+            textAnchor="middle">
+            {`$${tooltipPos.value}`}
+          </TextSVG>
+        </Svg>
+      </View>
+    ) : null;
+  };
+  const onDataPointClick = data => {
+    let isSamePoint = tooltipPos.x === data.x && tooltipPos.y === data.y;
+    isSamePoint
+      ? setTooltipPos(previousState => {
+          return {
+            ...previousState,
+            value: data.value,
+            visible: !previousState.visible,
+          };
+        })
+      : setTooltipPos({x: data.x, value: data.value, y: data.y, visible: true});
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -129,17 +199,19 @@ const Spending = ({navigation}) => {
           <Image source={threedot} style={styles.back} />
         </View>
         <ScrollView horizontal={true}>
-        <LineChart
-          data={datset}
-          width={screenWidth * 1.5}
-          height={256}
-          chartConfig={chartConfig}
-          bezier
-          withVerticalLines={false}
-          verticalLabelRotation={0}
-          formatYLabel={formatYLabel}
-          fromZero={true}
-        />
+          <LineChart
+            data={datset}
+            width={screenWidth * 1.5}
+            height={256}
+            chartConfig={chartConfig}
+            bezier
+            withVerticalLines={false}
+            verticalLabelRotation={0}
+            formatYLabel={formatYLabel}
+            fromZero={true}
+            decorator={decorator}
+            onDataPointClick={onDataPointClick}
+          />
         </ScrollView>
         <View>
           <View style={styles.budgetCard}>
@@ -258,14 +330,14 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 16,
     lineHeight: 20,
-    width:'auto'
+    width: 'auto',
   },
   header: {
     flexDirection: 'row',
     marginHorizontal: 10,
     marginVertical: '8%',
     alignItems: 'center',
-    width:screenWidth*0.9,
+    width: screenWidth * 0.9,
     justifyContent: 'space-between',
   },
   textCont: {
